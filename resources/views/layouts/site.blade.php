9a4c7e0b3bbed96c0a8d682b6dea99fe552b7fee
Ray <rayrlam@yahoo.com.hk>
2022-05-13 16:10:11 +0100

    update

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -1,104 +1,87 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>{{ config('app.name', 'Quotation') }}</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        
+        <!-- Font Awesome if you need it
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+        -->
+        
+        <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css"/>
+        <!--Replace with your tailwind.css once created-->
 
-        <title>Quotation</title>
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-gray-500{--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.dark\:text-gray-500{--tw-text-opacity:1;color:#6b7280;color:rgba(107,114,128,var(--tw-text-opacity))}}
-        </style>
-
-        <style>
-
-            body { font-family: 'Nunito', sans-serif; }
-            #items {
-                font-family: Arial, Helvetica, sans-serif;
-                border-collapse: collapse;
-                width: 100%;
-            }
-
-            #items td, #items th {
-                border: 1px solid #ddd;
-                padding: 8px;
-                padding-left: 20px;
-            }
-
-            #items tr:nth-child(even){background-color: #f2f2f2;}
-
-            #items tr:hover {background-color: #ddd;}
-
-            #items th {
-                padding-top: 12px;
-                padding-bottom: 12px;
-                text-align: left;
-                background-color: #04AA6D;
-                color: white;
-            }
-            .button.cal {
-                background-color: #4CAF50;
-                border: none;
-                color: white;
-                padding: 10px 30px;
-                text-align: center;
-                text-decoration: none;
-                display: inline-block;
-                font-size: 16px;
-                cursor: pointer;
-            }
+        <!-- Animation CSS-->
+        <style>		
+            /* ----------------------------------------------
+            * Generated by Animista
+            * w: http://animista.net, t: @cssanimista
+            * ---------------------------------------------- */
+            
+            /* .slide-in-bottom{-webkit-animation:slide-in-bottom .5s cubic-bezier(.25,.46,.45,.94) both;animation:slide-in-bottom .5s cubic-bezier(.25,.46,.45,.94) both}
+            .slide-in-bottom-h1{-webkit-animation:slide-in-bottom .5s cubic-bezier(.25,.46,.45,.94) .5s both;animation:slide-in-bottom .5s cubic-bezier(.25,.46,.45,.94) .5s both}
+            .slide-in-bottom-subtitle{-webkit-animation:slide-in-bottom .5s cubic-bezier(.25,.46,.45,.94) .75s both;animation:slide-in-bottom .5s cubic-bezier(.25,.46,.45,.94) .75s both}
+            .fade-in{-webkit-animation:fade-in 1.2s cubic-bezier(.39,.575,.565,1.000) 1s both;animation:fade-in 1.2s cubic-bezier(.39,.575,.565,1.000) 1s both}
+            .bounce-top-icons{-webkit-animation:bounce-top .9s 1s both;animation:bounce-top .9s 1s both}
+             */
+            /* @-webkit-keyframes slide-in-bottom{0%{-webkit-transform:translateY(1000px);transform:translateY(1000px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}@keyframes slide-in-bottom{0%{-webkit-transform:translateY(1000px);transform:translateY(1000px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}
+            @-webkit-keyframes bounce-top{0%{-webkit-transform:translateY(-45px);transform:translateY(-45px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in;opacity:1}24%{opacity:1}40%{-webkit-transform:translateY(-24px);transform:translateY(-24px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in}65%{-webkit-transform:translateY(-12px);transform:translateY(-12px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in}82%{-webkit-transform:translateY(-6px);transform:translateY(-6px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in}93%{-webkit-transform:translateY(-4px);transform:translateY(-4px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in}25%,55%,75%,87%{-webkit-transform:translateY(0);transform:translateY(0);-webkit-animation-timing-function:ease-out;animation-timing-function:ease-out}100%{-webkit-transform:translateY(0);transform:translateY(0);-webkit-animation-timing-function:ease-out;animation-timing-function:ease-out;opacity:1}}@keyframes bounce-top{0%{-webkit-transform:translateY(-45px);transform:translateY(-45px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in;opacity:1}24%{opacity:1}40%{-webkit-transform:translateY(-24px);transform:translateY(-24px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in}65%{-webkit-transform:translateY(-12px);transform:translateY(-12px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in}82%{-webkit-transform:translateY(-6px);transform:translateY(-6px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in}93%{-webkit-transform:translateY(-4px);transform:translateY(-4px);-webkit-animation-timing-function:ease-in;animation-timing-function:ease-in}25%,55%,75%,87%{-webkit-transform:translateY(0);transform:translateY(0);-webkit-animation-timing-function:ease-out;animation-timing-function:ease-out}100%{-webkit-transform:translateY(0);transform:translateY(0);-webkit-animation-timing-function:ease-out;animation-timing-function:ease-out;opacity:1}}
+            @-webkit-keyframes fade-in{0%{opacity:0}100%{opacity:1}}@keyframes fade-in{0%{opacity:0}100%{opacity:1}}
+                     */
         </style>
     </head>
-    <body class="antialiased">
-        <div class="w-full  flex items-top justify-center  bg-gray-100 dark:bg-gray-900 sm:items-center">
-        
-            <div class="sm:px-6">
-                @if (Route::has('login'))
-                    <div class="">
 
-                        <a href="{{ url('/') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Welcome</a>
+    <body class="leading-normal tracking-normal text-gray-900" style="font-family: 'Source Sans Pro', sans-serif;">
 
-                     
-                        @auth
-                            <a href="{{ url('/dashboard') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-                        @else
-                            <a href="{{ route('login') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                            @endif
-                        @endauth
+        <div class="bg-right bg-cover" style="background-image:url('liquid-cheese.svg');">
+         
+            <!--Nav-->
+            <!-- <div class="w-full container mx-auto p-6">
+                    
+                <div class="w-full flex items-center justify-between">
+                    <a class="flex items-center text-indigo-400 no-underline hover:no-underline font-bold text-2xl lg:text-4xl"  href="#"> 
+                        <svg class="h-8 fill-current text-indigo-600 pr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm-5.6-4.29a9.95 9.95 0 0 1 11.2 0 8 8 0 1 0-11.2 0zm6.12-7.64l3.02-3.02 1.41 1.41-3.02 3.02a2 2 0 1 1-1.41-1.41z"/></svg> APP
+                    </a>
+                    
+                    <div class="flex w-1/2 justify-end content-center">		
+                        <a class="inline-block text-blue-300 no-underline hover:text-indigo-800 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4" data-tippy-content="@twitter_handle" href="https://twitter.com/intent/tweet?url=#">
+                            <svg class="fill-current h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M30.063 7.313c-.813 1.125-1.75 2.125-2.875 2.938v.75c0 1.563-.188 3.125-.688 4.625a15.088 15.088 0 0 1-2.063 4.438c-.875 1.438-2 2.688-3.25 3.813a15.015 15.015 0 0 1-4.625 2.563c-1.813.688-3.75 1-5.75 1-3.25 0-6.188-.875-8.875-2.625.438.063.875.125 1.375.125 2.688 0 5.063-.875 7.188-2.5-1.25 0-2.375-.375-3.375-1.125s-1.688-1.688-2.063-2.875c.438.063.813.125 1.125.125.5 0 1-.063 1.5-.25-1.313-.25-2.438-.938-3.313-1.938a5.673 5.673 0 0 1-1.313-3.688v-.063c.813.438 1.688.688 2.625.688a5.228 5.228 0 0 1-1.875-2c-.5-.875-.688-1.813-.688-2.75 0-1.063.25-2.063.75-2.938 1.438 1.75 3.188 3.188 5.25 4.25s4.313 1.688 6.688 1.813a5.579 5.579 0 0 1 1.5-5.438c1.125-1.125 2.5-1.688 4.125-1.688s3.063.625 4.188 1.813a11.48 11.48 0 0 0 3.688-1.375c-.438 1.375-1.313 2.438-2.563 3.188 1.125-.125 2.188-.438 3.313-.875z"></path></svg>
+                        </a>
+                        <a class="inline-block text-blue-300 no-underline hover:text-indigo-800 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 " data-tippy-content="#facebook_id" href="https://www.facebook.com/sharer/sharer.php?u=#">
+                            <svg class="fill-current h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M19 6h5V0h-5c-3.86 0-7 3.14-7 7v3H8v6h4v16h6V16h5l1-6h-6V7c0-.542.458-1 1-1z"></path></svg>
+                        </a>
                     </div>
-                @endif
+                    
+                </div>
 
-               
+            </div> -->
+
+            <!--Main-->
+            <div class="container pt-8 px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
                 
-                <div class="flex justify-center sm:justify-start sm:pt-0">
-                    {{ $header }}
+                <!--Left Col-->
+                <div class="flex flex-col px-2 w-full justify-center lg:items-start">
+                    <h1 class="text-3xl md:text-5xl text-purple-800 font-bold leading-tight text-center md:text-left">{{ $header }}</h1>
+                    <p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">{{ $slot }}</p>
                 </div>
-
-                <div class="mt-4 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-                    <div class="flex ">
-                        {{ $slot }}
-             
-                    </div>
-                </div>               
-
-                <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
-                    <div class="text-center text-sm text-gray-500 sm:text-left">
-                        <div class="flex items-center">
-                        </div>
-                    </div>
-
-                    <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-                    </div>
+                
+                <!--Footer-->
+                <div class="w-full pt-8 pb-8 text-sm text-center md:text-left">
+                    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                 </div>
+        
             </div>
         </div>
+
+        <!-- jQuery if you need it
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        -->
+
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,36 +1,36 @@
 <x-site-layout>
     <x-slot name="header">
         <div class="flex justify-center sm:justify-start sm:pt-0">
-            <h1 class="ml-4">
-                {{ __('Technical Test - Development Task') }}    
-            </h1>
+             
+            {{ __('Development Task') }}    
+             
         </div>
     </x-slot>
 
-    <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
-                            
-        <div class="ml-4 text-xl mt-4 font-semibold text-gray-900 dark:text-white">
-            <p>
+    <div class="pb-16">
+
+        <div class="text-lg lg:mt-4   text-gray-900 dark:text-white">
+            <p class="mb-4">
                 If you are reasonably familiar with Symfony, this task should take approximately 3 hours (but don't worry if it takes more or less time than that).
             </p>
-            <p>
+            <p class="mb-4">
                 Create a Symfony project (either Symfony 4 or 5) for an API endpoint using DRY and SOLID object-oriented PHP, following PSR-12. The endpoint should accept a JSON payload containing the following fields: age, postcode, regNo. For example, the body of the request might look like this:
             </p>
-            <p>
-                {
-                    "age": 20,
-                    "postcode": "PE3 8AF",
-                    "regNo": "PJ63 LXR"
-                }        
-            </p>
+         
+<pre class="bg-blue-600 text-gray-200 p-4 rounded-lg">
+{
+    "age": 20,
+    "postcode": "PE3 8AF",
+    "regNo": "PJ63 LXR"
+}</pre>        
+        
         </div>
 
-
-        <div class="ml-4 text-xl mt-4 font-semibold text-gray-900 dark:text-white">
-            <p>
+        <div class="text-lg mt-4   text-gray-900 dark:text-white">
+            <p class="mb-4">
                 Your code should make a call to a third party API to look up the vehicle registration number and return an ABI code. Note this does not need to be a real API call - you can mock the response, but still show an example of how you could go about connecting to a third party API.
             </p>
-            <p>    
+            <p class="mb-4">
                 Use the attached quotation.sql to create a MySQL database which contains rating factors for various ages, postcode areas, and vehicle ABI codes. When a request hits your API, it should start with a base premium of £500 (this can be hard-coded), then find the rating factors to apply for age, postcode area, and ABI code (assume a rating factor of 1 if the value is not in the database). Multiply the premium by each rating factor in turn to obtain a total premium and return an appropriate JSON response. 
             </p>
             <p>    
@@ -38,91 +38,115 @@
             </p>    
         </div>
 
-        <h2>
+        <h2 class="text-2xl mt-8 font-bold">
             What we are looking for:
         </h2>
- 
-        <div class="ml-4 text-xl mt-4 font-semibold text-gray-900 dark:text-white">
-            <ul>        
-                <li>SOLID principles observed</li>
-                <li>PSR-12 followed</li>
-                <li>Good separation of concerns (especially between controller and model layers)</li>
-                <li>Rating factors extendable (hint: allow the rating factors to be specified in the dependency injection container)</li>
-                <li>No security vulnerabilities</li>
-                <li>No commented out code except where used to demonstrate a point (eg. for the mock API call)</li>
-                <li>Code should be executable (no mis-typed variable names, missing use statements, etc.)</li>
-                <li>Code should be concise and well laid out - no excessive use of blank lines, unused use statements, etc.</li>
+    
+        <div class="text-lg mt-4  text-gray-900 dark:text-white">
+            <ul class="lg:list-disc">        
+                <li class="lg:ml-4">SOLID principles observed</li>
+                <li class="lg:ml-4">PSR-12 followed</li>
+                <li class="lg:ml-4">Good separation of concerns (especially between controller and model layers)</li>
+                <li class="lg:ml-4">Rating factors extendable (hint: allow the rating factors to be specified in the dependency injection container)</li>
+                <li class="lg:ml-4">No security vulnerabilities</li>
+                <li class="lg:ml-4">No commented out code except where used to demonstrate a point (eg. for the mock API call)</li>
+                <li class="lg:ml-4">Code should be executable (no mis-typed variable names, missing use statements, etc.)</li>
+                <li class="lg:ml-4">Code should be concise and well laid out - no excessive use of blank lines, unused use statements, etc.</li>
             </ul>
         </div>
-        
-        <h2>
+            
+        <h2 class="text-2xl mt-8 font-bold">
             Remarks
         </h2>
  
-        <div class="ml-4 text-xl mt-4 font-semibold text-gray-900 dark:text-white">
-            <ul>
-                <li>Laravel Installation - <a class="text-gray-700" href="https://laravel.com/docs/9.x/installation" target="_blank">https://laravel.com/docs/9.x/installation</a></li>
-                <li>Create Database quotation</li>
-                <li>Edit .env at DB_DATABASE, DB_USERNAME & DB_PASSWORD for database connection</li>
-                <li>Run Laravel Command
-                    <div style="margin-top:20px; background-color:#091e42; color:#fefefe; width:90%; padding: 10px 20px">
-<pre>
-/*
-** Create all tables for this app
-*/ 
-php artisan migrate</pre>        
+        <div class="text-lg mt-4   text-gray-900 dark:text-white">
+            <ul class="break-all">
+                <li class="ml-4">Laravel Installation - <a class="text-gray-700" href="https://laravel.com/docs/9.x/installation" target="_blank">https://laravel.com/docs/9.x/installation</a></li>
+                <li class="ml-4">Create Database quotation</li>
+                <li class="ml-4">Edit .env at DB_DATABASE, DB_USERNAME & DB_PASSWORD for database connection</li>
+                <li class="ml-4">Run Laravel Command
+                    <div class="mt-4 bg-blue-600 text-gray-200 p-6 rounded-lg">
+                        <div class="text-gray-400">
+                            /*<br>
+                            ** Create all tables for this app<br>
+                            */
+                        </div>
+                        php artisan migrate        
                     </div>
-                    <div style="margin-top:20px; background-color:#091e42; color:#fefefe; width:90%; padding: 10px 20px">
-<pre>
-/*
-** Create the dumpy data for this app
-*/    
-php artisan db:seed</pre>        
-                    </div>
-                    <div style="margin-top:20px; background-color:#091e42; color:#fefefe; width:90%; padding: 10px 20px">
-<pre>
-/*
-** Run the QuoteTest to check for errors
-*/    
-php artisan test</pre>        
-                    </div>
-                </li>
-                <li style="margin-top:20px">
-                    Main File
-                    <ul>
-                        <li>
-                            Controller: App\Http\Controllers\Api\V1\QuoteV1Controller
-                        </li>
-                        <li>
-                            Interface: App\Http\Controllers\Api\V1\RatingFactor\RatingFactorInterface
-                        </li>
-                        <li>
-                            Rating Factor Classes: 
-                            <ul>
-                                <li>App\Http\Controllers\Api\V1\RatingFactor\Abicode</li>
-                                <li>App\Http\Controllers\Api\V1\RatingFactor\Age</li>
-                                <li>App\Http\Controllers\Api\V1\RatingFactor\Postcode</li>
-                                <li>App\Http\Controllers\Api\V1\RatingFactor\Premium</li>
-                            </ul>
+                    <div class="mt-8 bg-blue-600 text-gray-100 p-6 rounded-lg">
 
-                            Other Classes: 
-                            <ul>
-                                <li>App\Http\Controllers\Api\V1\Calculator</li>
-                                <li>App\Http\Controllers\Api\V1\QuoteRepository</li>
-                            </ul>
-                        </li>
-                        <li>
-                            Helpers: App\Helpers\QuoteHelper
-                        </li>
-                        <li>
-                            Tests: Tests\Unit\QuoteTest
-                        </li>
-                    </ul>
+                        <div class="text-gray-400">
+                            /*<br>
+                            ** Create the dumpy data for this app<br>
+                            */
+                        </div>
+                        php artisan db:seed --class=DataSeeder        
+                    </div>
+                    <div class="mt-8 bg-blue-600 text-gray-100 p-6 rounded-lg">
+                        <div class="text-gray-400">
+                            /*<br>
+                            ** Run test to check for errors<br>
+                            */
+                        </div>
+                        php artisan test        
+                    </div>
                 </li>
-                <li>
-                    Api Route: '/api/v1/quoting'
-                </li>
-            </ul>  
+            </ul>
         </div>
-    </div>
+                
+        <h2 class="text-2xl mt-8 font-bold">
+            Main Files
+        </h2>    
+        <ul class="break-all">
+       
+            <li class="text-indigo-700">App\Http\Controllers\Api\V1</li>
+            <li class="ml-4">
+                Controller:
+            </li>
+            <li class="ml-8 pb-4">QuoteV1Controller</li>
+            <li class="ml-4">
+                Other Classes:
+            </li>
+            <li class="ml-4"></li>
+            <li class="ml-8">Calculator</li>
+            <li class="ml-8 pb-4">QuoteRepository</li>
+ 
+       
+            <li class="text-indigo-700">App\Http\Controllers\Api\V1\RatingFactor</li>
+            <li class="ml-4">
+                Interface:
+            </li>
+            <li class="ml-8 pb-4">
+                RatingFactorInterface
+            </li>
+            <li class="">
+                <ul class="pb-4">
+                    <li class="ml-4">
+                        Rating Factor Classes:
+                    </li>
+                    <li class="ml-8">Abicode</li>
+                    <li class="ml-8">Age</li>
+                    <li class="ml-8">Postcode</li>
+                    <li class="ml-8">Premium</li>
+                </ul>
+            </li>
+            <li class="text-indigo-700">App\Helpers</li>
+            <li class="ml-4">
+                Helpers:
+            </li>
+            <li class="ml-8 pb-4">
+                QuoteHelper
+            </li>
+            <li class="text-indigo-700">Tests\Unit</li>
+            <li class="ml-4">
+                Tests:
+            </li>
+            <li class="ml-8 pb-4">
+                QuoteTest
+            </li>
+            <li class="lg:ml-4">
+                Api Route: <i class="text-indigo-700">/api/v1/quoting</i>
+            </li>
+        </ul>
+    </div>    
 </x-site-layout>
